5f071404529e09152edb27b962e6fc26e83b65b4
Felipe <luizbrutante@gmail.com>
2022-07-17 15:20:12 -0300

    Requisitos 17, 18 e 19

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const botao = document.getElementById('button');
 const email = document.getElementById('user');
 const senha = document.getElementById('senha');
+const enviar = document.getElementById('submit-btn');
+const concorda = document.getElementById('agreement');
+enviar.disabled = true;
 
 botao.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -9,3 +12,12 @@ botao.addEventListener('click', () => {
     alert('Email ou senha inválidos.');
   }
 });
+
+function ok() {
+  if (concorda.checked === true) {
+    enviar.disabled = false;
+  } else if (concorda.checked === false) {
+    enviar.disabled = true;
+  }
+}
+concorda.addEventListener('click', ok);
